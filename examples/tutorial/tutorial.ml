2a98459be0fd43ad27a841615ebdfbd762b69553
mrmr1993 <mr_1993@hotmail.co.uk>
2019-07-19 18:21:03 +0100

    Implement tutorial solutions

--- examples/tutorial/tutorial.ml
+++ examples/tutorial/tutorial.ml
@@ -80,7 +80,10 @@ let assert_is_square_root_of_9 (x : Field.Var.t) : (unit, _) Checked.t =
 (* In this field, it happens to be the case that -3 is a square. *)
 let () = assert (Field.is_square (Field.of_int (-3)))
 
-let assert_is_cube_root_of_1 (x : Field.Var.t) = failwith "Exercise 1"
+let assert_is_cube_root_of_1 (x : Field.Var.t) =
+  let%bind x2 = Field.Checked.mul x x in
+  let%bind x3 = Field.Checked.mul x2 x in
+  Field.Checked.Assert.equal x3 (Field.Var.constant Field.one)
 
 let cube_root_of_1 =
   let open Field in
@@ -115,7 +118,7 @@ let exercise1 () =
   printf !"is %{sexp:Field.t} a cube root of 1? %b\n%!" cube_root_of_1 is_valid
 
 (* Exercise 1: Comment this out when you're ready to test it! *)
-(* let () = exercise1 () *)
+let () = exercise1 ()
 
 let exercise2 () =
   (* Now let's prove that there are two cube roots of 1. *)
@@ -129,18 +132,26 @@ let exercise2 () =
      Now you try: Creating a data spec, keypair, proof, and verifying that proof
      for `distinct_cube_roots_of_1`.
    *)
-  let another_cube_root_of_1 = failwith "x^3 = 1, find x" in
-  let input () = failwith "Exercise 2: Data_spec here" in
-  let keypair = failwith "Exercise 2: Keypair here" in
-  let proof = failwith "Exercise 2: Proof" in
-  let is_valid = failwith "Exercise 2: Verify" in
+  let another_cube_root_of_1 = Field.mul cube_root_of_1 cube_root_of_1 in
+  let input () = Data_spec.[Field.typ; Field.typ] in
+  let keypair =
+    generate_keypair ~exposing:(input ()) distinct_cube_roots_of_1
+  in
+  let proof =
+    prove (Keypair.pk keypair) (input ()) () distinct_cube_roots_of_1
+      cube_root_of_1 another_cube_root_of_1
+  in
+  let is_valid =
+    verify proof (Keypair.vk keypair) (input ()) cube_root_of_1
+      another_cube_root_of_1
+  in
   printf
     !"Are %{sexp:Field.t} and %{sexp:Field.t} two distinct cube roots of 1? %b\n\
       %!"
     cube_root_of_1 another_cube_root_of_1 is_valid
 
 (* Exercise 2: Comment this out when you're ready to test it! *)
-(* let () = exercise2 () *)
+let () = exercise2 ()
 
 (* We can encode other data types in terms of the underlying fields. An
    extremely useful one is boolean values -- true and false.
@@ -171,7 +182,13 @@ let if_both x y a b =
    that returns zero if the input is zero, or the inverse of the input
    otherwise.
 *)
-let zero_or_inverse (x : Field.Var.t) = failwith "Exercise 3"
+let zero_or_inverse (x : Field.Var.t) =
+  let%bind b = Field.Checked.equal x (Field.Var.constant Field.zero) in
+  let%bind invertable =
+    Field.Checked.if_ b ~then_:(Field.Var.constant Field.one) ~else_:x
+  in
+  let%bind inverse = Field.Checked.inv invertable in
+  Field.Checked.if_ b ~then_:x ~else_:inverse
 
 let exercise3 () =
   (* Unchecked reference implementation. *)
@@ -185,8 +202,8 @@ let exercise3 () =
     let%bind y = zero_or_inverse x in
     Field.Checked.Assert.equal y expected_value
   in
-  let input () = failwith "Exercise 3: Data_spec here" in
-  let keypair = failwith "Exercise 3: Keypair here" in
+  let input () = Data_spec.[Field.typ; Field.typ] in
+  let keypair = generate_keypair ~exposing:(input ()) matches_unchecked in
   let proof x =
     prove (Keypair.pk keypair) (input ()) () matches_unchecked x
       (zero_or_inverse_unchecked x)
@@ -194,14 +211,18 @@ let exercise3 () =
   let proof_0 = proof Field.zero in
   let proof_1 = proof Field.one in
   let proof_15 = proof (Field.of_int 15) in
-  let is_valid_0 = failwith "Exercise 3: Verify proof_0" in
-  let is_valid_1 = failwith "Exercise 3: Verify proof_1" in
-  let is_valid_15 = failwith "Exercise 3: Verify proof_15" in
+  let is_valid proof x =
+    verify proof (Keypair.vk keypair) (input ()) x
+      (zero_or_inverse_unchecked x)
+  in
+  let is_valid_0 = is_valid proof_0 Field.zero in
+  let is_valid_1 = is_valid proof_1 Field.one in
+  let is_valid_15 = is_valid proof_15 (Field.of_int 15) in
   printf "Matched expected output for\n0? %b\n1? %b\n15? %b\n" is_valid_0
     is_valid_1 is_valid_15
 
 (* Exercise 3: Comment this out when you're ready to test it! *)
-(* let () = exercise3 () *)
+let () = exercise3 ()
 
 (* So far, the only data we have passed to [prove] have been Field elements.
    To pass other kinds of data, we will need to change the values that we pass
@@ -222,8 +243,8 @@ let exercise4 () =
     let%bind z = Boolean.(x && y) in
     Boolean.Assert.is_true z
   in
-  let input () = failwith "Exercise 4: Data_spec here" in
-  let keypair = failwith "Exercise 4: Keypair here" in
+  let input () = Data_spec.[Boolean.typ; Boolean.typ] in
+  let keypair = generate_keypair ~exposing:(input ()) either in
   let proof = prove (Keypair.pk keypair) (input ()) () either true true in
   let is_valid proof x y = verify proof (Keypair.vk keypair) (input ()) x y in
   let proved x y = is_valid proof x y in
@@ -232,7 +253,7 @@ let exercise4 () =
     (proved true true) (proved true false)
 
 (* Exercise 4: Comment this out when you're ready to test it! *)
-(* let () = exercise4 () *)
+let () = exercise4 ()
 
 (* We can encode richer data types in the same way; as long as we can tell
    [prove] how to turn our OCaml value into field elements with a [Typ.t], we
@@ -250,7 +271,11 @@ let exercise4 () =
 *)
 
 let product (l : Field.Var.t list) : (Field.Var.t, _) Checked.t =
-  failwith "Exercise 5"
+  List.fold l
+    ~init:(Checked.return (Field.Var.constant Field.one))
+    ~f:(fun product term ->
+      let%bind product = product in
+      Field.Checked.mul product term )
 
 let product_equals (l : Field.Var.t list) (expected_total : Field.Var.t) =
   let%bind total = product l in
@@ -260,10 +285,14 @@ let product_unchecked (l : Field.t list) =
   List.fold ~init:Field.one ~f:Field.mul l
 
 let exercise5 () =
-  let input () = failwith "Exercise 5: Data_spec here" in
-  let keypair = failwith "Exercise 5: Keypair here" in
-  let proof l expected_total = failwith "Exercise 5: Proof" in
-  let is_valid proof l expected_total = failwith "Exercise 5: Verify" in
+  let input () = Data_spec.[Typ.list ~length:5 Field.typ; Field.typ] in
+  let keypair = generate_keypair ~exposing:(input ()) product_equals in
+  let proof l expected_total =
+    prove (Keypair.pk keypair) (input ()) () product_equals l expected_total
+  in
+  let is_valid proof l expected_total =
+    verify proof (Keypair.vk keypair) (input ()) l expected_total
+  in
   let proved (l : int list) =
     let l : Field.t list = List.map ~f:Field.of_int l in
     let expected_total = product_unchecked l in
@@ -272,7 +301,7 @@ let exercise5 () =
   printf "Does product [1; 2; 3; 4; 5] = 120? %b\n" (proved [1; 2; 3; 4; 5])
 
 (* Exercise 5: Comment this out when you're ready to test it! *)
-(* let () = exercise5 () *)
+let () = exercise5 ()
 
 (* Exercise 6:
    Adapt your solution to exercise 5 to create a checked version of
@@ -282,10 +311,32 @@ let exercise5 () =
 let product_triple ((x, y, z) : Field.t * Field.t * Field.t) : Field.t =
   Field.(x * y * z)
 
-let exercise6 () = failwith "Exercise 6"
+let product_triple_checked
+    ((x, y, z) : Field.Var.t * Field.Var.t * Field.Var.t) :
+    (Field.Var.t, _) Checked.t =
+  let%bind xy = Field.Checked.mul x y in
+  Field.Checked.mul xy z
+
+let product_triple_equals triple (expected_total : Field.Var.t) =
+  let%bind total = product_triple_checked triple in
+  Field.Checked.Assert.equal total expected_total
+
+let exercise6 () =
+  let input () =
+    Data_spec.[Typ.tuple3 Field.typ Field.typ Field.typ; Field.typ]
+  in
+  let keypair = generate_keypair ~exposing:(input ()) product_triple_equals in
+  let proof = prove (Keypair.pk keypair) (input ()) () product_triple_equals in
+  let is_valid proof = verify proof (Keypair.vk keypair) (input ()) in
+  let proved (x, y, z) =
+    let triple = (Field.of_int x, Field.of_int y, Field.of_int z) in
+    let expected_total = product_triple triple in
+    is_valid (proof triple expected_total) triple expected_total
+  in
+  printf "Does product (2, 3, 5) = 30? %b\n" (proved (2, 3, 5))
 
 (* Exercise 6: Comment this out when you're ready to test it! *)
-(* let () = exercise6 () *)
+let () = exercise6 ()
 
 (* At this point, we have covered one way to pass data to a checked
    computation: describing what types the data has in our [Data_spec], and
@@ -312,7 +363,11 @@ let exercise6 () = failwith "Exercise 6"
 *)
 
 let product (l : Field.Var.t list) : (Field.Var.t, _) Checked.t =
-  failwith "Exercise 7"
+  List.fold l
+    ~init:(Checked.return (Field.Var.constant Field.one))
+    ~f:(fun product term ->
+      let%bind product = product in
+      Field.Checked.mul product term )
 
 let product_unchecked (l : Field.t list) =
   List.fold ~init:Field.one ~f:Field.mul l
@@ -337,10 +392,10 @@ let product_equals (l : Field.Var.t list) =
   Field.Checked.Assert.equal total expected_total
 
 let exercise7 () =
-  let input () = failwith "Exercise 7: Data_spec here" in
-  let keypair = failwith "Exercise 7: Keypair here" in
-  let proof l = failwith "Exercise 7: Proof" in
-  let is_valid proof l = failwith "Exercise 7: Verify" in
+  let input () = Data_spec.[Typ.list ~length:5 Field.typ] in
+  let keypair = generate_keypair ~exposing:(input ()) product_equals in
+  let proof = prove (Keypair.pk keypair) (input ()) () product_equals in
+  let is_valid proof = verify proof (Keypair.vk keypair) (input ()) in
   let proved (l : int list) =
     let l : Field.t list = List.map ~f:Field.of_int l in
     is_valid (proof l) l
@@ -351,7 +406,7 @@ let exercise7 () =
     (proved [1; 2; 3; 4; 5])
 
 (* Exercise 7: Comment this out when you're ready to test it! *)
-(* let () = exercise7 () *)
+let () = exercise7 ()
 
 (* Now we can say that a value 'exists' without giving it away to the verifier,
    but we have to say right there in our checked computation how we're going to
@@ -376,7 +431,13 @@ type _ Request.t += Choose_two_from_list : 'a list -> ('a * 'a) Request.t
 
 let in_list (x : Field.Var.t) (l : Field.Var.t list) =
   (* Assert that x is equal to one of the values in l. *)
-  failwith "Exercise 8"
+  let%bind x_in_l =
+    List.fold l ~init:(Checked.return Boolean.false_) ~f:(fun in_prefix y ->
+        let%bind in_prefix = in_prefix in
+        let%bind x_eq_y = Field.Checked.equal x y in
+        Boolean.(in_prefix || x_eq_y) )
+  in
+  Boolean.Assert.is_true x_in_l
 
 let choose_two_from_list (l : Field.Var.t list) =
   exists
@@ -385,7 +446,8 @@ let choose_two_from_list (l : Field.Var.t list) =
       As_prover.(
         (* Read the values from l and create a [Choose_two_from_list] request
            with them. *)
-        failwith "Exercise 8")
+        let%map l = read Typ.(list ~length:(List.length l) Field.typ) l in
+        Choose_two_from_list l)
 
 let chosen_two_different (l : Field.Var.t list) =
   let%bind choice1, choice2 = choose_two_from_list l in
@@ -394,8 +456,8 @@ let chosen_two_different (l : Field.Var.t list) =
   Field.Checked.Assert.not_equal choice1 choice2
 
 let exercise8 () =
-  let input () = failwith "Exercise 8: Data_spec here" in
-  let keypair = failwith "Exercise 8: Keypair here" in
+  let input () = Data_spec.[Typ.list ~length:5 Field.typ] in
+  let keypair = generate_keypair ~exposing:(input ()) chosen_two_different in
   let secret1 = 1 in
   let secret2 = 3 in
   let handled_chosen_two_different l =
@@ -421,7 +483,7 @@ let exercise8 () =
     (proved [1; 2; 3; 4; 5])
 
 (* Exercise 8: Comment this out when you're ready to test it! *)
-(* let () = exercise8 () *)
+let () = exercise8 ()
 
 module Exercise9 = struct
   (* We can define a matrix over some ring as follows *)
@@ -466,7 +528,7 @@ module Exercise9 = struct
     let open Field in
     [|[|of_int 1; of_int 2|]; [|of_int 3; of_int 4|]; [|of_int 5; of_int 6|]|]
 
-  (* let () = printf !"Result %{sexp: Mat.t}\n%!" (Mat.mul a b) *)
+  let () = printf !"Result %{sexp: Mat.t}\n%!" (Mat.mul a b)
 
   (* Exercise 9:
      To bring everything together, we want to prove something more substantial.
@@ -481,22 +543,80 @@ module Exercise9 = struct
      (Feel free to hard-code a matrix here to start with; it makes it much
      easier to track down any small bugs!)
   *)
-  let random_matrix () = failwith "Exercise 9: Random matrix"
+  let random_matrix ~rows ~cols =
+    Array.init rows ~f:(fun _ -> Array.init cols ~f:(fun _ -> Field.random ()))
+
+  module Array_iter (M : Monad.S2) = struct
+    let rec loop min max ~f =
+      let open M.Let_syntax in
+      if min < max then
+        let%bind () = f min in
+        loop (min + 1) max ~f
+      else M.return ()
+
+    let init count ~f =
+      if count <= 0 then M.return [||]
+      else
+        let open M.Let_syntax in
+        (* Calculate the initial value, so we have something to pre-fill the
+           array with. For most types, OCaml will use pointers and this
+           pre-filling will have no allocation overhead.
+        *)
+        let%bind x0 = f 0 in
+        let array = Array.create ~len:count x0 in
+        (* Overwrite the values in subsequent array positions to the desired
+           values.
+        *)
+        let%map () =
+          loop 1 count ~f:(fun i ->
+              let%map x = f i in
+              array.(i) <- x )
+        in
+        array
+
+    let iter array ~f = loop 0 (Array.length array) ~f:(fun i -> f array.(i))
+
+    let iter2 array1 array2 ~f =
+      let len = Array.length array1 in
+      assert (len = Array.length array2) ;
+      loop 0 len ~f:(fun i -> f array1.(i) array2.(i))
+  end
+
+  module Checked_array = Array_iter (Checked)
 
   module Mat_checked = struct
     type t = Field.Var.t array array
+
+    let rows t = Array.length t
+
+    let row t i = t.(i)
+
+    let col t i = Array.map t ~f:(fun xs -> xs.(i))
+
+    let cols t = Array.length t.(0)
 
     (* Next, we need to make a checked version of [Matrix.mul] from above.
        This should feel familiar: we did a very similar thing when we were
        finding the product of a list!
      *)
     let mul : t -> t -> (t, _) Checked.t =
-     fun a b -> failwith "Exercise 9: Write mul"
+     fun a b ->
+      assert (cols a = rows b) ;
+      Checked_array.init (rows a) ~f:(fun i ->
+          Checked_array.init (cols b) ~f:(fun j ->
+              Array.fold2_exn (row a i) (col b j)
+                ~init:(Checked.return (Field.Var.constant Field.zero))
+                ~f:(fun acc aik bkj ->
+                  let%map acc = acc and p = Field.Checked.mul aik bkj in
+                  Field.Var.add acc p ) ) )
 
     module Assert = struct
       (* Now, we want a way to say that two matrices are equal. *)
       let equal : t -> t -> (unit, _) Checked.t =
-       fun a b -> failwith "Exercise 9: Write Assert.equal"
+       fun a b ->
+        Checked_array.iter2 a b ~f:(fun a b ->
+            Checked_array.iter2 a b ~f:(fun a b ->
+                Field.Checked.Assert.equal a b ) )
     end
 
     (* Bonus: Try an adjust the Matrix definition above to functor over a
@@ -505,27 +625,60 @@ module Exercise9 = struct
      *)
   end
 
+  module Store_array = Array_iter (Typ_monads.Store)
+  module Read_array = Array_iter (Typ_monads.Read)
+  module Alloc_array = Array_iter (Typ_monads.Alloc)
+
   (* Create a [Typ.t] to let us convert between [Mat.t] and [Mat_checked.t].
 
      NB: SNARKs require fixed-size inputs, so this Typ.t will have to describe
      matrices of a fixed-size. Make sure that this size matches your
      [random_matrix]'s output size!
   *)
-  let typ () : (Mat_checked.t, Mat.t) Typ.t = failwith "Exercise 9: Write typ"
+  let typ ~rows ~cols : (Mat_checked.t, Mat.t) Typ.t =
+    { store=
+        (fun mat ->
+          assert (Array.length mat = rows) ;
+          Store_array.init rows ~f:(fun i ->
+              let row = mat.(i) in
+              assert (Array.length row = cols) ;
+              Store_array.init cols ~f:(fun j -> Typ.Store.store row.(j)) ) )
+    ; read=
+        (fun mat ->
+          assert (Array.length mat = rows) ;
+          Read_array.init rows ~f:(fun i ->
+              let row = mat.(i) in
+              assert (Array.length row = cols) ;
+              Read_array.init cols ~f:(fun j -> Typ.Read.read row.(j)) ) )
+    ; alloc=
+        Alloc_array.init rows ~f:(fun _i ->
+            Alloc_array.init cols ~f:(fun _j -> Typ.Alloc.alloc) )
+    ; check= (fun _ -> Checked.return ()) }
 
   (* Fill in this function to check that [sqrt_x] squares to [x]. *)
   let assert_is_sqrt (x : Mat_checked.t) (sqrt_x : Mat_checked.t) =
-    failwith "Exercise 9: Write assert_is_sqrt"
+    let%bind sqrt_x2 = Mat_checked.mul sqrt_x sqrt_x in
+    Mat_checked.Assert.equal x sqrt_x2
+
+  type _ Request.t += Get_matrix_sqrt : Mat.t Request.t
 
   (* Add a new kind of [Request.t] that asks for the square root matrix that we
      will generate later, and use [assert_is_sqrt] to check that it squares to
      the matrix we've been given. *)
-  let assert_exists_sqrt (x : Mat_checked.t) =
-    failwith "Exercise 9: Write assert_exists_sqrt"
+  let assert_exists_sqrt ~length (x : Mat_checked.t) =
+    let%bind sqrt_x =
+      exists
+        (typ ~rows:length ~cols:length)
+        ~request:(As_prover.return Get_matrix_sqrt)
+    in
+    assert_is_sqrt x sqrt_x
 
-  let input () = Data_spec.[typ ()]
+  let input ~rows ~cols = Data_spec.[typ ~rows ~cols]
 
-  let keypair () = generate_keypair ~exposing:(input ()) assert_exists_sqrt
+  let keypair ~length =
+    generate_keypair
+      ~exposing:(input ~rows:length ~cols:length)
+      (assert_exists_sqrt ~length)
 
   (* Build a proof.
      This should consist of:
@@ -537,18 +690,38 @@ module Exercise9 = struct
      NB: This function should return the public input and the proof, so that
      the input can be passed to the verifier.
   *)
-  let proof () : Mat.t * Proof.t = failwith "Exercise 9: Write proof"
+  let proof ~length keypair : Mat.t * Proof.t =
+    let sqrt_x = random_matrix ~rows:length ~cols:length in
+    let x = Mat.mul sqrt_x sqrt_x in
+    let assert_exists_sqrt x =
+      handle (assert_exists_sqrt ~length x) (fun (With {request; respond}) ->
+          match request with
+          | Get_matrix_sqrt ->
+              respond (Provide sqrt_x)
+          | _ ->
+              respond Unhandled )
+    in
+    let proof =
+      prove (Keypair.pk keypair)
+        (input ~rows:length ~cols:length)
+        () assert_exists_sqrt x
+    in
+    (x, proof)
 
   (* Verify the proof. *)
-  let is_valid proof (x : Mat.t) = failwith "Exercise 9: Write is_valid"
+  let is_valid ~length keypair proof (x : Mat.t) =
+    verify proof (Keypair.vk keypair) (input ~rows:length ~cols:length) x
 
-  let run () =
-    let x, proof = proof () in
+  let run ~length =
+    let keypair = keypair ~length in
+    let x, proof = proof ~length keypair in
     printf
       !"Does %{sexp: Mat.t} have a square root?\n%b%!"
-      x (is_valid proof x)
+      x
+      (is_valid ~length keypair proof x)
 end
 
 (* Exercise 9: Comment this out when you're ready to test it! *)
-(* let () = Exercise9.run () *)
+let () = Exercise9.run ~length:5
+
 (* TODO: To_bits of_bits *)
